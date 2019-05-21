fc8213d657a7eb3d1cb6a6cad0000b1908238ecb
nava565 <navaj565@oddth.postbox.in.th>
2019-05-21 16:22:40 +0700

    Dfdfd

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
+var bot *linebot.Client
+
 func main() {
-	fmt.Println("Create File Main.go Success")
+	fmt.Println("Init...")
 	handleRequest()
 }
 
@@ -23,28 +26,33 @@ func statusPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "It's ok!")
 }
 
-func botFunc(w http.ResponseWriter, r *http.Request) {
-	var body struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
+func botFunc(w http.ResponseWriter, req *http.Request) {
+	if bot == nil {
+		bot, _ = linebot.New(
+			"ae6P1wQm9pDtBXz1TQNnAqWJSUHvIiUl0GWPJNvLK8MQxYuPIaqaP+Kea9H6QcnyVCyw2iJILvy00zXyV/B9nIB+NAeP9P9da7HZxbk0atcm2tYeuXngrKaMBMWwMy3msa5PEluN2bGu0JI7enTELwdB04t89/1O/w1cDnyilFU=",
+			"097b8b8f50bbe56a02030db996b7b69d",
+		)
 	}
 
-	url := "https://api.line.me/v2/bot/message/reply"
-	accToken := "ae6P1wQm9pDtBXz1TQNnAqWJSUHvIiUl0GWPJNvLK8MQxYuPIaqaP+Kea9H6QcnyVCyw2iJILvy00zXyV/B9nIB+NAeP9P9da7HZxbk0atcm2tYeuXngrKaMBMWwMy3msa5PEluN2bGu0JI7enTELwdB04t89/1O/w1cDnyilFU="
-	var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", " Bearer "+accToken)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	events, err := bot.ParseRequest(req)
 	if err != nil {
-		panic(err)
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
 	}
-	defer resp.Body.Close()
-
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	json.NewEncoder(w).Encode(body)
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}
 }
 
 func getPort() string {
