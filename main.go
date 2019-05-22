b0bd858485b5fb89bc946f146f47fca75c5efcbf
nava565 <navaj565@oddth.postbox.in.th>
2019-05-22 11:26:44 +0700

    test

--- main.go
+++ main.go
@@ -32,11 +32,7 @@ func lastRequest(w http.ResponseWriter, r *http.Request) {
 }
 
 func botFunc(w http.ResponseWriter, r *http.Request) {
-	var body struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
-	}
-
+	var lineEvents = LineEvents{}
 	bodyBytes, _ := ioutil.ReadAll(r.Body)
 	lastReq = string(bodyBytes)
 	//print raw response body for debugging purposes
@@ -45,22 +41,26 @@ func botFunc(w http.ResponseWriter, r *http.Request) {
 	//reset the response body to the original unread state
 	r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
 
-	url := "https://api.line.me/v2/bot/message/reply"
-	accToken := "ae6P1wQm9pDtBXz1TQNnAqWJSUHvIiUl0GWPJNvLK8MQxYuPIaqaP+Kea9H6QcnyVCyw2iJILvy00zXyV/B9nIB+NAeP9P9da7HZxbk0atcm2tYeuXngrKaMBMWwMy3msa5PEluN2bGu0JI7enTELwdB04t89/1O/w1cDnyilFU="
-	var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", " Bearer "+accToken)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
+	if err := json.NewDecoder(r.Body).Decode(&lineEvents); err != nil {
+		return
 	}
-	defer resp.Body.Close()
+
+	// url := "https://api.line.me/v2/bot/message/reply"
+	// accToken := "ae6P1wQm9pDtBXz1TQNnAqWJSUHvIiUl0GWPJNvLK8MQxYuPIaqaP+Kea9H6QcnyVCyw2iJILvy00zXyV/B9nIB+NAeP9P9da7HZxbk0atcm2tYeuXngrKaMBMWwMy3msa5PEluN2bGu0JI7enTELwdB04t89/1O/w1cDnyilFU="
+	// var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
+	// req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	// req.Header.Set("Content-Type", "application/json")
+	// req.Header.Set("Authorization", " Bearer "+accToken)
+
+	// client := &http.Client{}
+	// resp, err := client.Do(req)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer resp.Body.Close()
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	json.NewEncoder(w).Encode(body)
+	fmt.Fprint(w, "It's oks!")
 }
 
 func getPort() string {
@@ -70,4 +70,26 @@ func getPort() string {
 		fmt.Println("No Port In Heroku" + port)
 	}
 	return ":" + port
+}
+
+type LineEvents struct {
+	Events      []Events `json:"events"`
+	Destination string   `json:"destination"`
+}
+type Source struct {
+	UserID  string `json:"userId"`
+	GroupID string `json:"groupId"`
+	Type    string `json:"type"`
+}
+type Message struct {
+	Type string `json:"type"`
+	ID   string `json:"id"`
+	Text string `json:"text"`
+}
+type Events struct {
+	Type       string  `json:"type"`
+	ReplyToken string  `json:"replyToken"`
+	Source     Source  `json:"source"`
+	Timestamp  int64   `json:"timestamp"`
+	Message    Message `json:"message"`
 }
