8f10b6eed995ea5a9c588bc825da0c7b5b90902e
nava565 <navaj565@oddth.postbox.in.th>
2019-05-22 11:11:57 +0700

    sfdsf

--- main.go
+++ main.go
@@ -1,50 +1,66 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"log"
+	"io/ioutil"
 	"net/http"
 	"os"
-
-	"github.com/line/line-bot-sdk-go/linebot"
 )
 
+var lastReq string
+
 func main() {
-	bot, err := linebot.New(
-		"ae6P1wQm9pDtBXz1TQNnAqWJSUHvIiUl0GWPJNvLK8MQxYuPIaqaP+Kea9H6QcnyVCyw2iJILvy00zXyV/B9nIB+NAeP9P9da7HZxbk0atcm2tYeuXngrKaMBMWwMy3msa5PEluN2bGu0JI7enTELwdB04t89/1O/w1cDnyilFU=",
-		"097b8b8f50bbe56a02030db996b7b69d",
-	)
-	if err != nil {
-		log.Fatal(err)
+	fmt.Println("Create File Main.go Success")
+	handleRequest()
+}
+
+func handleRequest() {
+	http.HandleFunc("/callback", botFunc)
+	http.HandleFunc("/status", statusPage)
+	http.HandleFunc("/lastrq", lastRequest)
+	http.ListenAndServe(getPort(), nil)
+}
+
+func statusPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "It's ok!")
+}
+
+func lastRequest(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, lastReq)
+}
+
+func botFunc(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
 	}
 
-	// Setup HTTP Server for receiving requests from LINE platform
-	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
-		events, err := bot.ParseRequest(req)
-		if err != nil {
-			if err == linebot.ErrInvalidSignature {
-				w.WriteHeader(400)
-			} else {
-				w.WriteHeader(500)
-			}
-			return
-		}
-		for _, event := range events {
-			if event.Type == linebot.EventTypeMessage {
-				switch message := event.Message.(type) {
-				case *linebot.TextMessage:
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-						log.Print(err)
-					}
-				}
-			}
-		}
-	})
-	// This is just sample code.
-	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
-	if err := http.ListenAndServe(":"+getPort(), nil); err != nil {
-		log.Fatal(err)
+	bodyBytes, _ := ioutil.ReadAll(r.Body)
+	lastReq := string(bodyBytes)
+	//print raw response body for debugging purposes
+	fmt.Println("\n\n", lastReq, "\n\n")
+
+	//reset the response body to the original unread state
+	r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+
+	url := "https://api.line.me/v2/bot/message/reply"
+	accToken := "ae6P1wQm9pDtBXz1TQNnAqWJSUHvIiUl0GWPJNvLK8MQxYuPIaqaP+Kea9H6QcnyVCyw2iJILvy00zXyV/B9nIB+NAeP9P9da7HZxbk0atcm2tYeuXngrKaMBMWwMy3msa5PEluN2bGu0JI7enTELwdB04t89/1O/w1cDnyilFU="
+	var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", " Bearer "+accToken)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
 	}
+	defer resp.Body.Close()
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(body)
 }
 
 func getPort() string {
