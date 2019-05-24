f843fcdd35070a92d0c5f4e44d53bef33a312d3d
nava565 <navaj565@oddth.postbox.in.th>
2019-05-24 15:20:18 +0700

    fsdf

--- main.go
+++ main.go
@@ -7,12 +7,15 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 var lastReq string
+var m map[string]string
 
 func main() {
-	fmt.Println("Create File Main.go Success")
+	fmt.Println("Init success")
+	m := make(map[string]string)
 	handleRequest()
 }
 
@@ -47,24 +50,23 @@ func botFunc(w http.ResponseWriter, r *http.Request) {
 
 	for _, lineEvent := range lineEvents.Events {
 		if lineEvent.Type == "message" {
-			reply := &Reply{ReplyToken: lineEvent.ReplyToken, Message: []Message{Message{Type: "text", Text: lineEvent.Message.Text}}}
-			fmt.Print(reply)
 
-			url := "https://api.line.me/v2/bot/message/reply"
-			accToken := "ae6P1wQm9pDtBXz1TQNnAqWJSUHvIiUl0GWPJNvLK8MQxYuPIaqaP+Kea9H6QcnyVCyw2iJILvy00zXyV/B9nIB+NAeP9P9da7HZxbk0atcm2tYeuXngrKaMBMWwMy3msa5PEluN2bGu0JI7enTELwdB04t89/1O/w1cDnyilFU="
+			reply := &Reply{ReplyToken: lineEvent.ReplyToken, Message: []Message{Message{Type: "text", Text: ""}}}
+			s := strings.Split("เพิ่มเบอร์ @nawa 0959351389", " ")
+
+			if s[0] == "เพิ่มเบอร์" {
+				fmt.Println(s[1])
+				m[s[1]] = s[2]
+			}
+
+			if s[0] == "เบอร์" {
+				if m[s[1]] != "" {
+					reply.Message[0].Text = m[s[1]]
+				}
+			}
 
 			jsonStr, _ := json.Marshal(*reply)
-
-			req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-			req.Header.Set("Content-Type", "application/json")
-			req.Header.Set("Authorization", " Bearer "+accToken)
-
-			client := &http.Client{}
-			resp, err := client.Do(req)
-			if err != nil {
-				panic(err)
-			}
-			defer resp.Body.Close()
+			sendMessage(jsonStr)
 
 			lastReq = string(jsonStr)
 		}
@@ -72,6 +74,21 @@ func botFunc(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	fmt.Fprint(w, "It's oks!")
+}
+
+func sendMessage(message []byte) {
+	url := "https://api.line.me/v2/bot/message/reply"
+	accToken := "ae6P1wQm9pDtBXz1TQNnAqWJSUHvIiUl0GWPJNvLK8MQxYuPIaqaP+Kea9H6QcnyVCyw2iJILvy00zXyV/B9nIB+NAeP9P9da7HZxbk0atcm2tYeuXngrKaMBMWwMy3msa5PEluN2bGu0JI7enTELwdB04t89/1O/w1cDnyilFU="
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(message))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", " Bearer "+accToken)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
 }
 
 func getPort() string {
